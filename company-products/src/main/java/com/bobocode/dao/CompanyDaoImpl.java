6fa1cd0f2e725721bfa420edf42e2d936af6f259
Vlad Kondratenko <omgkanamikun@gmail.com>
2018-09-28 09:30:35 +0300

    update

--- company-products/src/main/java/com/bobocode/dao/CompanyDaoImpl.java
+++ company-products/src/main/java/com/bobocode/dao/CompanyDaoImpl.java
@@ -1,10 +1,14 @@
 package com.bobocode.dao;
 
+import com.bobocode.exception.CompanyDaoException;
 import com.bobocode.model.Company;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.Objects;
 
 public class CompanyDaoImpl implements CompanyDao {
+
     private EntityManagerFactory entityManagerFactory;
 
     public CompanyDaoImpl(EntityManagerFactory entityManagerFactory) {
@@ -13,16 +17,29 @@ public class CompanyDaoImpl implements CompanyDao {
 
     @Override
     public Company findByIdFetchProducts(Long id) {
-        return null;
+        Objects.requireNonNull(id);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            return entityManager.createQuery("select c from Company c join fetch c.products where c.id = :id", Company.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new CompanyDaoException("error, rollback!", e);
+        } finally {
+            entityManager.close();
+        }
     }
 }
 
 //    @Override
 //    public Company findByIdFetchProducts(Long id) {
-//        return readWithinTransaction(entityManager -> entityManager
-//                .createQuery("select c from Company c join fetch c.products where c.id = :id", Company.class)
-//                .setParameter("id", id)
-//                .getSingleResult());
+//        return readWithinTx(entityManager ->
+//                entityManager
+//                        .createQuery("select c from Company c join fetch c.products where c.id = :id", Company.class)
+//                        .setParameter("id", id)
+//                        .getSingleResult()
+//        );
 //    }
 //
 //    public <T> T readWithinTransaction(Function<EntityManager, T> readFunction) {
